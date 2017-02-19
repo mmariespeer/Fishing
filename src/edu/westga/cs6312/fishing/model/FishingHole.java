3942b6f55f5a97f820216ce152481a8ca512b9ca
mmari <mmari@DESKTOP-OVK3T16.att.net>
2017-02-19 12:01:47 -0500

    FishingHole class developed and tested
    All test's passing

--- src/edu/westga/cs6312/fishing/model/FishingHole.java
+++ src/edu/westga/cs6312/fishing/model/FishingHole.java
@@ -1,5 +1,37 @@
 package edu.westga.cs6312.fishing.model;
 
 public class FishingHole {
-
+	private FishType fish;
+	private int holeLocation;
+	
+	public FishingHole(int holeLocation) {
+		this.holeLocation = holeLocation;
+		setupFishingHole();
+	}
+	
+	public String getLocation() {
+		return "Fishing hole at [" + this.holeLocation + "]";
+	}
+	
+	public FishType getFish() {
+		return this.fish;
+	}
+	
+	public String toString() {
+		return getLocation() + " with " + this.fish.toString();
+	}
+	
+	private void setupFishingHole(){
+		int chance = (int) Math.random() * 100;
+		if (chance >= 50) {
+			this.fish = null;
+		} else {
+			this.fish = addFish();
+		}
+	}
+	
+	private FishType addFish() {
+		FishType smallFish = new SmallFish();
+		return smallFish;
+	}
 }
--- src/edu/westga/cs6312/fishing/tests/FishingHoleWhenGetLocation.java
+++ src/edu/westga/cs6312/fishing/tests/FishingHoleWhenGetLocation.java
@@ -1,14 +1,29 @@
 package edu.westga.cs6312.fishing.tests;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import edu.westga.cs6312.fishing.model.*;
 
 public class FishingHoleWhenGetLocation {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCreateFishHoleGetLocation() {
+		FishingHole theFishingHole = new FishingHole(0);
+		assertEquals(theFishingHole.getLocation(), "Fishing hole at [0]");
 	}
+	
+	@Test
+	public void testCreateFishHoleGetLocation10() {
+		FishingHole theFishingHole = new FishingHole(10);
+		assertEquals(theFishingHole.getLocation(), "Fishing hole at [10]");
+	}
+	
+	@Test
+	public void testCreateFishHoleGetLocation57() {
+		FishingHole theFishingHole = new FishingHole(57);
+		assertEquals(theFishingHole.getLocation(), "Fishing hole at [57]");
+	}
+	
+	
 
 }
